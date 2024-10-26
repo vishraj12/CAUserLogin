4f80b953ab8ca5c348970264fd1f863717a95721
vishraj12 <vishaka.rajani@mail.utoronto.ca>
2024-10-26 16:21:17 -0400

    Task 1

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -19,7 +19,7 @@ import view.ViewManager;
 /**
  * The version of Main with an external database used to persist user data.
  */
-public class MainwithInMemory {
+public class MainWithInMemory {
 
     /**
      * The main method for starting the program with an external database used to persist user data.
