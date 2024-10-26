5c57491c02d6e5f9dfc898755c76c63f982b9788
vishraj12 <vishaka.rajani@mail.utoronto.ca>
2024-10-26 16:29:27 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name given name
+     */
+    void setCurrentUser(String name);
 }
